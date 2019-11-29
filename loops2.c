cfe691238360cd140269b4ccc2d2e34d0beba6d6
Jiao <s1928253@ed.ac.uk>
2019-11-29 03:20:06 +0000

    final version1

--- loops2.c
+++ loops2.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 #define N 729
-#define reps 1000 
+#define reps 1000
 #include <omp.h> 
 
 double a[N][N], b[N][N], c[N];
@@ -15,7 +15,6 @@ void loop1chunk(int, int);
 void loop2chunk(int, int);
 void valid1(void);
 void valid2(void);
-void fred(int niterations, int nthreads);
 
 int main(int argc, char *argv[]) {
 
@@ -38,19 +37,19 @@ int main(int argc, char *argv[]) {
 
 
 
-    init2();
+  init2();
 
-    start2 = omp_get_wtime();
+  start2 = omp_get_wtime();
 
-    for (r=0; r<reps; r++){
-      runloop(2);
-    }
+  for (r=0; r<reps; r++){
+    runloop(2);
+  }
 
-    end2  = omp_get_wtime();
+  end2  = omp_get_wtime();
 
-    valid2();
+  valid2();
 
-    printf("Total time for %d reps of loop 2 = %f\n",reps, (float)(end2-start2));
+  printf("Total time for %d reps of loop 2 = %f\n",reps, (float)(end2-start2));
 
 } 
 
@@ -69,20 +68,20 @@ void init2(void){
   int i,j, expr;
 
   for (i=0; i<N; i++){
-    expr =  i%( 3*(i/30) + 1);  // i=0, 0  i=1, 1.1 i=2, 1.2
+    expr =  i%( 3*(i/30) + 1);
 
-    if (expr == 0) {  // 给jmax赋值1/729
+    if (expr == 0) {
       jmax[i] = N;
     }
     else {
       jmax[i] = 1;
     }
-    c[i] = 0.0;     // 给c赋值0
+    c[i] = 0.0;
   }
 
   for (i=0; i<N; i++){
     for (j=0; j<N; j++){
-      b[i][j] = (double) (i*j+1) / (double) (N*N);  // 给b重新赋值
+      b[i][j] = (double) (i*j+1) / (double) (N*N);
     }
   }
 }
@@ -90,57 +89,95 @@ void init2(void){
 void runloop(int loopid)  {
     
   int *ipt;
+  int *lo;
+  int *hi;
 
-#pragma omp parallel num_threads(8) default(none) shared(loopid, ipt)
+#pragma omp parallel default(none) shared(loopid, ipt, lo, hi)
   {
     int myid  = omp_get_thread_num();
     int nthreads = omp_get_num_threads();
 #pragma omp single
   {
     ipt = (int*)malloc(sizeof(int)*nthreads);
+    lo = (int*)malloc(sizeof(int)*nthreads);
+    hi = (int*)malloc(sizeof(int)*nthreads);
   }
-    ipt[myid] = (int) ceil((double)N/(double)nthreads); // 729 / 4 = 183  假如10个进程，729 / 10 = 73
+  ipt[myid] = (int) ceil((double)N/(double)nthreads); // 729 / 4 = 183
+  lo[myid] = myid*ipt[myid];
+  hi[myid] = (myid+1)*ipt[myid];
+  if (hi[myid] > N) hi[myid] = N;
 
-    int chunksize;
-    int lo, hi;
+  int lo_exec = 0;     // temporary value
+  int hi_exec = 0;     // temporary value
+  int next_thread = 0; // loop condition, represents which thread will be executed next
+  int chunksize = 0;   // chunksize
+  int remaining = 0;   // remaining iterations in a thread
+  /* 
+   * These two variables are for after running out local set,
+   * begining to search other sets.
+   * max: the number of most iterations of some thread
+   * maxthread: the id of thread with most iterations
+   */
+  int max;
+  int maxthread;
 
-    lo = myid*ipt[myid];
-    hi = (myid+1)*ipt[myid];
-    if (hi > N) hi = N;
+  while(next_thread != -1) {
 
-    while(ipt[myid]>0) {
-      chunksize = (int)ceil((double)ipt[myid]/(double)nthreads);
-
-      hi = lo + chunksize;  // 183 366 549 732        // 73 146 219 292 365 438 511 584 657 730
-      if (hi > N) hi = N;  // 183 366 549 729
-
-      //      printf("lo on thread %d is %d\n", myid, lo);
-      //      printf("hi on thread %d is %d\n", myid, hi);
-
-
-
-
-
-
-
-
-
-      switch (loopid) {
-        case 1: loop1chunk(lo,hi); break;
-        case 2: loop2chunk(lo,hi); break;
-      }
-      ipt[myid] -= chunksize;
-      lo += chunksize;
-      //      printf("this is thread %d, ipt is %d\n", myid, ipt[myid]);
+    switch (loopid) {
+      case 1: loop1chunk(lo_exec,hi_exec); break;
+      case 2: loop2chunk(lo_exec,hi_exec); break;
     }
-  }
+#pragma omp critical
+  {
+    /* There is still remaining in local set, prioritize local tasks */
+    if (hi[myid] - lo[myid] > 0) {
+      next_thread = myid;
+
+      remaining = hi[next_thread]-lo[next_thread]; 
+      chunksize = (int)ceil((double)remaining/(double)nthreads);
+
+      lo_exec = lo[next_thread];
+      hi_exec = lo_exec + chunksize;
+      lo[next_thread] = lo[next_thread] + chunksize;
+    }
+    /* There is no remaining in local set, finding in other threads */
+    else {
+      max = 0;
+      maxthread = -1;
+
+      int i;
+      /* search thread with the most iterations */
+      for (i=0; i<nthreads; i++) {
+	if (myid == i)
+	  continue;
+	if (hi[i] - lo[i] > max) {
+	  max = hi[i] - lo[i];  // max iterations
+	  maxthread = i;  // thread index
+	}
+      }
+      next_thread = maxthread;
+      if (next_thread != -1) {
+	int remaining = hi[next_thread]-lo[next_thread];  
+	chunksize = (int)ceil((double)remaining/(double)nthreads);
+
+	lo_exec = lo[next_thread];
+	hi_exec = lo_exec + chunksize;
+	lo[next_thread] = lo[next_thread] + chunksize;
+      }
+    }
+  } // critical
+  } // while
+  } // parallel
+  free(ipt);
+  free(lo);
+  free(hi);
 }
 
 void loop1chunk(int lo, int hi) { 
   int i,j; 
 
-  for (i=lo; i<hi; i++){    // i = 0 183 366 549; i < 183 366 549 729
-    for (j=N-1; j>i; j--){  // j = 728; j 728 727, ..., 545(取值)     j = 728; j 728, 727, ..., 0
+  for (i=lo; i<hi; i++){
+    for (j=N-1; j>i; j--){
       a[i][j] += cos(b[i][j]);
     } 
   }
@@ -150,7 +187,7 @@ void loop2chunk(int lo, int hi) {
   int i,j,k;
   double rN2;
 
-  rN2 = 1.0 / (double) (N*N);  // 0.00000188167
+  rN2 = 1.0 / (double) (N*N);
 
   for (i=lo; i<hi; i++){
     for (j=0; j<jmax[i]; j++){
